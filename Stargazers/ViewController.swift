58e997d20138fd29c8ab57365167d74ffefd9080
mttcrsp <mttcrsp@gmail.com>
2017-12-23 09:07:53 +0100

    Initial Commit

---
+++ Stargazers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stargazers
+//
+//  Created by Matteo Crespi on 23/12/2017.
+//  Copyright © 2017 Matteo Crespi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

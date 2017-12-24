ce05629ad90cb60aa708766bcc08d68c51521ece
mttcrsp <mttcrsp@gmail.com>
2017-12-24 12:15:39 +0100

    Display stargazers count for each repository

--- Stargazers/Views/RepositoryTableViewCell.swift
+++ Stargazers/Views/RepositoryTableViewCell.swift
@@ -6,8 +6,18 @@
 import UIKit
 
 final class RepositoryTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     func configure(with repository: Repository) {
         textLabel?.text = repository.name
-        detailTextLabel?.text = "\(repository.stargazersCount) ✨"
+        detailTextLabel?.text = "\(repository.stargazersCount) stargazers"
+        detailTextLabel?.textColor = .lightGray
     }
 }

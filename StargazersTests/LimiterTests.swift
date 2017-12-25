b0bac7a4bdcf7f8501eb9bcf347250d4008a883b
mttcrsp <mttcrsp@gmail.com>
2017-12-25 01:36:26 +0100

    Add Limiter tests

---
+++ StargazersTests/LimiterTests.swift
@@ -0,0 +1,52 @@
+//
+//  Created by Matteo Crespi on 25/12/2017.
+//  Copyright © 2017 Matteo Crespi. All rights reserved.
+//
+
+@testable
+import Stargazers
+import XCTest
+
+class LimiterTests: XCTestCase {
+    
+    func testLimitsCorrectly() {
+        // GIVEN: three blocks
+        let firstExpectation = self.expectation(description: "first")
+        let secondExpectation = self.expectation(description: "second")
+        let thirdExpectation = self.expectation(description: "third")
+        let first: (DispatchWorkItem) -> Void = { item in
+            self.delay(by: 0.2) { XCTAssertTrue(item.isCancelled) ; firstExpectation.fulfill() }
+        }
+        let second: (DispatchWorkItem) -> Void = { item in
+            self.delay(by: 0.2) { XCTAssertTrue(item.isCancelled) ; secondExpectation.fulfill() }
+        }
+        let third: (DispatchWorkItem) -> Void = { item in
+            self.delay(by: 0.2) { XCTAssertFalse(item.isCancelled) ; thirdExpectation.fulfill() }
+        }
+        // WHEN: asked to perform each one of them before the previous one completes
+        let subject = Limiter()
+        subject.execute(first)
+        delay(by: 0.1) { subject.execute(second) }
+        delay(by: 0.2) { subject.execute(third) }
+        // THEN: it cancels first two and only the third one is completed
+        wait(for: [firstExpectation, secondExpectation, thirdExpectation], timeout: 5)
+    }
+    
+    func testDoesNotLimitCorrectly() {
+        // GIVEN: Two block
+        let firstExpectation = self.expectation(description: #function)
+        let secondExpectation = self.expectation(description: #function)
+        let first: (DispatchWorkItem) -> Void = { item in
+            self.delay(by: 0.05) { XCTAssertFalse(item.isCancelled) ; firstExpectation.fulfill() }
+        }
+        let second: (DispatchWorkItem) -> Void = { item in
+            self.delay(by: 0.05) { XCTAssertFalse(item.isCancelled) ; secondExpectation.fulfill() }
+        }
+        // WHEN: asked to execute one after the other
+        let subject = Limiter()
+        subject.execute(first)
+        delay(by: 0.1) { subject.execute(second) }
+        // THEN: it does not cancel either of them
+        wait(for: [firstExpectation, secondExpectation], timeout: 0.3)
+    }
+}
